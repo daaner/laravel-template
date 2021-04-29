4b9ddab417d8ba705b8bccb95125af04a0a0dc35
Andrey <daaner@users.noreply.github.com>
2021-04-29 22:49:53 +0000

    Apply fixes from StyleCI

--- app/Admin/Sections/Settings.php
+++ app/Admin/Sections/Settings.php
@@ -42,19 +42,19 @@ class Settings extends Section implements Initializable
       ->setHtmlAttribute('class', 'table-danger table-hover');
 
         $display->setColumns([
-      // AdminColumn::text('id', '#')
-      //   ->setWidth('50px'),
-      AdminColumn::link('name', 'Настройка')
-        ->setWidth('150px'),
-      AdminColumn::boolean('value', 'Состояние')
-        ->setWidth('130px')
-        ->setOrderable(true),
-      AdminColumn::text('description', 'Описание'),
-      AdminColumn::text('updated_at', 'Изменен', 'editors.name')
-        ->setWidth('160px')
-        ->setSearchable(false)
-        ->setOrderable(true),
-    ]);
+            // AdminColumn::text('id', '#')
+            //   ->setWidth('50px'),
+            AdminColumn::link('name', 'Настройка')
+              ->setWidth('150px'),
+            AdminColumn::boolean('value', 'Состояние')
+              ->setWidth('130px')
+              ->setOrderable(true),
+            AdminColumn::text('description', 'Описание'),
+            AdminColumn::text('updated_at', 'Изменен', 'editors.name')
+              ->setWidth('160px')
+              ->setSearchable(false)
+              ->setOrderable(true),
+        ]);
 
         return $display;
     }
@@ -62,33 +62,33 @@ class Settings extends Section implements Initializable
     public function onEdit($id)
     {
         $form = AdminForm::panel()->addBody([
-      AdminFormElement::columns()->addColumn([
-        AdminFormElement::text('name', 'Название')
-          ->setReadonly(1)
-          ->required(),
-        AdminFormElement::textarea('description', 'Описание')
-          ->setRows(3)
-          ->addValidationRule('max:190', __('adm.valid.max190')),
-        AdminFormElement::html('<hr>'),
-        AdminFormElement::checkbox('value', 'Состояние настройки'),
-      ], 8)->addColumn([
-        AdminFormElement::text('creators.name', 'Создал')
-          ->setReadonly(1),
-        AdminFormElement::text('updated_at', 'Создано')
-          ->setReadonly(1),
-        AdminFormElement::html('<hr>'),
-        AdminFormElement::text('editors.name', 'Редактировал')
-          ->setReadonly(1),
-        AdminFormElement::text('updated_at', 'Редакция')
-          ->setReadonly(1),
-      ]),
-    ]);
+            AdminFormElement::columns()->addColumn([
+                AdminFormElement::text('name', 'Название')
+                  ->setReadonly(1)
+                  ->required(),
+                AdminFormElement::textarea('description', 'Описание')
+                  ->setRows(3)
+                  ->addValidationRule('max:190', __('adm.valid.max190')),
+                AdminFormElement::html('<hr>'),
+                AdminFormElement::checkbox('value', 'Состояние настройки'),
+            ], 8)->addColumn([
+                AdminFormElement::text('creators.name', 'Создал')
+                  ->setReadonly(1),
+                AdminFormElement::text('updated_at', 'Создано')
+                  ->setReadonly(1),
+                AdminFormElement::html('<hr>'),
+                AdminFormElement::text('editors.name', 'Редактировал')
+                  ->setReadonly(1),
+                AdminFormElement::text('updated_at', 'Редакция')
+                  ->setReadonly(1),
+            ]),
+        ]);
 
         $form->getButtons()->setButtons([
-      // 'save'  => new Save(),
-      'save_and_close'  => new SaveAndClose(),
-      'cancel'          => (new Cancel()),
-    ]);
+            // 'save'  => new Save(),
+            'save_and_close'  => new SaveAndClose(),
+            'cancel'          => (new Cancel()),
+        ]);
 
         return $form;
     }
