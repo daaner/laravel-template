e2caf3d1e6dd92b945e1411bcd69065be1d5c580
Andrey <daaner@users.noreply.github.com>
2021-06-05 09:01:39 +0000

    Apply fixes from StyleCI

--- app/Admin/navigation.php
+++ app/Admin/navigation.php
@@ -3,46 +3,46 @@
 use SleepingOwl\Admin\Navigation\Page;
 
 return [
-  [
-    'title'    => 'Панель',
-    'icon'     => 'fas fa-tachometer-alt',
-    'url'      => route('admin.dashboard'),
-    'priority' => 50,
-  ],
-
-  // [
-  //   'title' => 'name cat',
-  //   'icon' => 'fa fa-users',
-  //   'priority' => 100,
-  //   'pages' =>
-  //   [
-  //     (new Page(\App\Models\Model::class))
-  //     ->setTitle('name')
-  //     ->setPriority(100),
-  //   ],
-  // ],
-
-  // [
-  //   'title' => 'Менеджер файлов',
-  //   'icon'  => 'fa fa-files-o',
-  //   'url'   => route('admin.filemanager'),
-  //   'priority' => 3800,
-  // ],
-
-  [
-    'title'    => 'Настройки',
-    'icon'     => 'fas fa-cogs',
-    'priority' => 5000,
-    'pages'    => [
-      (new Page(\App\Setting::class))
-        ->setPriority(100),
-      (new Page(\App\Models\Script::class))
-        ->setPriority(800),
-      (new Page(\App\User::class))
-        ->setPriority(900),
-      (new Page(\App\Role::class))
-        ->setPriority(1000),
+    [
+        'title'    => 'Панель',
+        'icon'     => 'fas fa-tachometer-alt',
+        'url'      => route('admin.dashboard'),
+        'priority' => 50,
     ],
-  ],
+
+    // [
+    //   'title' => 'name cat',
+    //   'icon' => 'fa fa-users',
+    //   'priority' => 100,
+    //   'pages' =>
+    //   [
+    //     (new Page(\App\Models\Model::class))
+    //     ->setTitle('name')
+    //     ->setPriority(100),
+    //   ],
+    // ],
+
+    // [
+    //   'title' => 'Менеджер файлов',
+    //   'icon'  => 'fa fa-files-o',
+    //   'url'   => route('admin.filemanager'),
+    //   'priority' => 3800,
+    // ],
+
+    [
+        'title'    => 'Настройки',
+        'icon'     => 'fas fa-cogs',
+        'priority' => 5000,
+        'pages'    => [
+            (new Page(\App\Setting::class))
+              ->setPriority(100),
+            (new Page(\App\Models\Script::class))
+              ->setPriority(800),
+            (new Page(\App\User::class))
+              ->setPriority(900),
+            (new Page(\App\Role::class))
+              ->setPriority(1000),
+        ],
+    ],
 
 ];
